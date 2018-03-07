3320d553c7771677f75422aa3f0b9e65d9ea49ea
Louis Medina <louismedina@Louiss-MacBook-Pro.local>
2018-03-07 10:48:49 -0500

    Angular.js | Component to Component Communication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// New
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NgForExampleComponent } from './ng-for-example/ng-for-example.component';
+import { MySignupFormComponent } from './my-signup-form/my-signup-form.component';
+import { QuoteItemComponent } from './quote-item/quote-item.component';
+import { QuoteListComponent } from './quote-list/quote-list.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgForExampleComponent,
+    MySignupFormComponent,
+    QuoteItemComponent,
+    QuoteListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    // New
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
